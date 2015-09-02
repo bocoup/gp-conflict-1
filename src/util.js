de4e9c4a186600a3c5260b74f5bf9e50b38d440b
Irene Ros <imirene@gmail.com>
2015-09-02 16:47:02 -0400

    Updating sketch

---
+++ src/util.js
@@ -0,0 +1,28 @@
+window.Util = {
+  /**
+  * Preloads images
+  */
+  preloadImage: function(url) {
+    var img = new Image();
+    img.src = url;
+  },
+
+  /**
+  * returns a 0 for a given value if it is NaN, otherwise
+  * returns the number.
+  * @param n number
+  * @returns number
+  */
+  zeroIfNan: function(n) {
+    return isNaN(n) ? 0 : +n;
+  }
+};
+
+/**
+* d3, move node to front
+*/
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+  this.parentNode.appendChild(this);
+  });
+};
